6445bcedcc99986024c6f8bc8d60a38384b7fcde
AndrewG <andrew@andrewg.xyz>
2023-11-01 19:33:55 -0400

    Moved express listen from express.js to server.js

--- mern_skeleton/server/express.js
+++ mern_skeleton/server/express.js
@@ -1,13 +1,15 @@
 import express from 'express'
-import http from 'http'
+import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
-import config from '../config/config'
-import Template from './../template'
 
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
+
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
 app.use(bodyParser.json())
@@ -17,13 +19,18 @@ app.use(compress())
 app.use(cors())
 app.use(helmet())
 
-app.get('/', (req, res) => {
-  return res.status(200).send(Template())
-})
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
-http.createServer(app).listen(config.port, '0.0.0.0', (err) => {
-  if (err) {
+app.use('/', authRoutes)
+app.use('/', userRoutes)
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error": err.name + ": " + err.message})
+  } else if (err) {
+    res.status(400).json({"error": err.name + ": " + err.message})
     console.log(err)
   }
-  console.info('Server started on port %s', config.port)
 })
+
+export default app
