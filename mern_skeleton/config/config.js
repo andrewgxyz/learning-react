961f41dcbf5b7f3cf745ba5a762ba1fc564a992f
AndrewG <andrew@andrewg.xyz>
2023-11-20 14:46:43 -0500

    Making sure the mongodb and nodejs containers are talking to each other right

--- mern_skeleton/config/config.js
+++ mern_skeleton/config/config.js
@@ -2,9 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 80,
   jwtSecret: process.env.JWT_SECRET || 'secret',
-  mongoUri: process.env.MONGO_URI || 
-    process.env.MONGO_HOST || 
-    'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mernproject'
+  mongoUri: 'mongodb://' + (process.env.DB_HOST || 'localhost') + ':' + (process.env.DB_PORT || '27017') + '/' + process.env.DB_DATABASE + '?authSource=admin'
 }
 
 export default config
